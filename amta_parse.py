c8d536088db90950b45cf93583f9e17e2ec6099f
Jackson <jmeyerlee@haverford.edu>
2018-03-06 17:18:28 -0500

    now works with ALL AMTA FORMATTED PDFS

--- amta_parse.py
+++ amta_parse.py
@@ -2,10 +2,18 @@ from os import listdir
 import tabula
 import pandas as pd
 import tab_formatting
+import re
 
+def fix_page(page_df):
+    if page_df.shape[1] == 13:
+        page_df.drop(columns=['Unnamed: 0', 'Unnamed: 2', 'Unnamed: 4', 'Unnamed: 6', 'Unnamed: 8', 'Unnamed: 10', 'Unnamed: 12'], inplace=True)
 
 def parse_amta(path):
-    df = tabula.read_pdf(path, java_options=["-Dfile.encoding=UTF8"], pages=[1,2], lattice=True, pandas_options={'error_bad_lines' : False})
+    p1 = tabula.read_pdf(path, java_options=["-Dfile.encoding=UTF8"], pages=[1], lattice=True, pandas_options={'error_bad_lines' : False})
+    p2 = tabula.read_pdf(path, java_options=["-Dfile.encoding=UTF8"], pages=[2], lattice=True, pandas_options={'error_bad_lines' : False})
+    fix_page(p1)
+    fix_page(p2)
+    df = pd.concat([p1,p2], ignore_index=True)
     try:
         df = df.drop(df.index[14]).reset_index(drop=True)
     except IndexError:
